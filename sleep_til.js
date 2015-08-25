e8d586efab3dc1bfddb219d0c3e38650e18e3672
Nathan Rugg <nmrugg@gmail.com>
2015-08-25 19:49:56 +0800

    Added wait range.

--- sleep_til.js
+++ sleep_til.js
@@ -1,15 +1,48 @@
 #!/usr/bin/env node
 
 var til = process.argv[2];
-var ms;
+var start;
+var stop;
+var after_wait;
 
-function get_from_time(t)
+function run_program()
+{
+    var spawn = require("child_process").spawn,
+        i,
+        len = process.argv.length,
+        program = process.argv[3],
+        args = [],
+        exce;
+    
+    function onerr(e)
+    {
+        console.log("Error detected:");
+        console.log(e);
+    }
+    
+    for (i = 4; i < len; i += 1) {
+        args.push(process.argv[i]);
+    }
+    
+    exce = spawn(program, args, {stdio: [0, 1, 2], cwd: process.cwd(), env: process.env}).on("error", onerr)/*.on("close", onclose)*/;
+    
+    setTimeout(function after_final_wait()
+    {
+        exce.kill("SIGTERM"); /// SIGTERM is sent by default, by why not specify it.
+    }, stop).unref(); /// unref() it because we don't need to stop it if it stops before the time.
+}
+
+function get_from_time(t, start_from)
 {
     var match = t.match(/^(\d+):(\d+)(?::(\d+))?(?:\s*(am|pm))?$/i);
     var d, h, m, dm, cm;
     
     if (match) {
-        d = new Date();
+        if (typeof start_from === "undefined") {
+            d = new Date();
+        } else {
+            d = new Date(start_from);
+        }
         
         h = parseInt(match[1], 10);
         m = parseInt(match[2], 10);
@@ -19,7 +52,7 @@ function get_from_time(t)
             s = d.getSeconds();
         }
         
-        /// 12 is really 0.
+        /// 12 is really zero.
         if (h === 12) {
             h = 0;
         }
@@ -44,23 +77,31 @@ function get_from_time(t)
 if (!til) {
     console.log("Need a time.");
     console.log("");
-    console.log("Usage: sleep_til TIME");
+    console.log("Usage: sleep_til TIME[-TIME PROGRAM args1 ... argsN]");
+    console.log("");
     console.log("  TIME can be the following:");
     console.log("  A timestamp:");
-    console.log("       1:00      (assumes AM)");
-    console.log("      14:30      (24 hour)");
-    console.log("       4:33am");
-    console.log("       1:30PM");
-    console.log("       1:30:01PM (with seconds)");
+    console.log("     1:00      (assumes AM)");
+    console.log("    14:30      (24 hour)");
+    console.log("     4:33am");
+    console.log("     1:30PM");
+    console.log("     1:30:01PM (with seconds)");
     console.log("  One of the following time words:");
-    console.log("       noon");
-    console.log("       midnight");
+    console.log("     noon");
+    console.log("     midnight");
     console.log("  Time to wait:");
-    console.log("       1s     (seconds)");
-    console.log("       5m     (minutes)");
-    console.log("       5min   (can use longer words)");
-    console.log("       3H     (hours, case insensitive)");
-    console.log("       3days  (days)");
+    console.log("     1s     (seconds)");
+    console.log("     5m     (minutes)");
+    console.log("     5min   (can use longer words)");
+    console.log("     3H     (hours, case insensitive)");
+    console.log("     3days  (days)");
+    console.log("");
+    console.log("  If a range is specified, it will execute a program for the specified amount of time:");
+    console.log("    11:00PM-6:00AM wget -c http://example.com/largefile.zip");
+    console.log("  (This will run \"wget\" (with the following args) from 11:00PM to 6:00AM)");
+    console.log("");
+    console.log("  If you don't specifiy a range, you will want to chain this with another program, like this:");
+    console.log("    sleep_til 5minutes; wget ...");
     console.log("");
     console.log("NOTE: When using a timestamp, it will use the current time's second value if not specified.");
     console.log("NOTE: The maximum time to wait is about 24 days.");
@@ -77,36 +118,58 @@ til = til.replace(/(\d)hours?$/, "$1h");
 til = til.replace(/(\d)min(?:ute)?s?$/, "$1m");
 til = til.replace(/(\d)sec(?:ond)?s?$/, "$1s");
 
-var last = til.slice(-1).toLowerCase();
-
-if (last === "d") {
-    ms = parseFloat(til) * 24 * 60 * 60 * 1000;
-} else if (last === "h") {
-    ms = parseFloat(til) * 60 * 60 * 1000;
-} else if (last === "m" && !/[pa]m$/i.test(til)) {
-    ms = parseFloat(til) * 60 * 1000;
-} else if (last === "s" && !/[pa]m$/i.test(til)) {
-    if (til.slice(-2, -1).toLowerCase() === "m") {
-        ms = parseFloat(til);
+function parse_time(til, start_from)
+{
+    var last = til.slice(-1).toLowerCase(),
+        ms;
+    
+    if (last === "d") {
+        ms = parseFloat(til) * 24 * 60 * 60 * 1000;
+    } else if (last === "h") {
+        ms = parseFloat(til) * 60 * 60 * 1000;
+    } else if (last === "m" && !/[pa]m$/i.test(til)) {
+        ms = parseFloat(til) * 60 * 1000;
+    } else if (last === "s" && !/[pa]m$/i.test(til)) {
+        if (til.slice(-2, -1).toLowerCase() === "m") {
+            ms = parseFloat(til);
+        } else {
+            ms = parseFloat(til) * 1000;
+        }
     } else {
-        ms = parseFloat(til) * 1000;
+        ms = get_from_time(til, start_from);
     }
-} else {
-    ms = get_from_time(til);
+    
+    if (isNaN(ms) || ms < 0) {
+        if (!isNaN(til)) {
+            ms = parseFloat(til);
+        }
+    }
+    
+    return ms;
 }
 
-if (isNaN(ms) || ms < 0) {
-    if (!isNaN(til)) {
-        ms = parseFloat(til);
-    } else {
-        return console.log("Invalid time");
+function get_times(til)
+{
+    var parts = til.split("-");
+    
+    start = parse_time(parts[0]);
+    if (parts[1]) {
+        ///TODO: We need this to be time AFTER start.
+        ///      1s-1s would be the same, but
+        ///      11:00AM-10:00AM would be 11:00AM one day and 10:00AM the next day.
+        stop = parse_time(parts[1], new Date().valueOf() + start);
     }
 }
+
+get_times(til);
 
 ///NOTE: 2147483648 will overflow and be the same as 0.
 ///      2147483647 will not; however, doing any thing close to 2147483647 still make node use a significant amount of CPU for some mysterious reason.
-if (ms > 2147400000) {
+if (start > 2147400000) {
     return console.log("Too long, sorry.");
+}
+if (typeof stop === "number" &&  stop > 2147400000) {
+    return console.log("End time too long, sorry.");
 }
 
 function human_readable_time(t)
@@ -139,6 +202,12 @@ function human_readable_time(t)
     return t + " " + unit + (t === 1 ? "" : "s");
 }
 
-console.log("Waiting for about " + human_readable_time(ms) + ".");
+console.log("Waiting for about " + human_readable_time(start) + ".");
 
-setTimeout(function () {}, ms);
+if (typeof stop !== "number") {
+    after_wait = function () {};
+} else {
+    after_wait = run_program;
+}
+
+setTimeout(after_wait, start);
